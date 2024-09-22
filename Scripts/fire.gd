306c078da597031e25e41b70f15d67dbfe56f2d8
Spunboing <diamondjellybean457@gmail.com>
2024-09-21 23:45:03 -0700

    random fire spawns in ee=very 5 seconds

--- Scripts/fire.gd
+++ Scripts/fire.gd
@@ -4,11 +4,20 @@ extends Node2D
 #max fire size: 100
 @export var size: float
 
+func _ready():
+	get_tree().call_group("fire", "disable")
+	
+
 func _process(delta: float) -> void:
 	size += delta * 1
 	scale = Vector2(size/25, size/25)
 	if scale.x + scale.y < 1:
-		queue_free()
+		set_visible(false)
+		set_process(false)
 
 func extinguish(amount: float):
 	size -= amount
+	
+func disable():
+	set_visible(false)
+	set_process(false)
---
+++ Scripts/fire_activation.gd
@@ -0,0 +1,28 @@
+extends Node
+
+@onready var fire_array = get_tree().get_nodes_in_group("fire")
+@onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	timer.start()
+
+
+func generate():
+	var fire_index = int(randf_range(0,228))
+	return fire_index
+	
+func enable(fuego):
+	if fuego.is_in_group("fire"): 
+		fuego.set_visible(true)
+		fuego.set_process(true)
+		_ready()
+	
+	
+func _on_timer_timeout() -> void:
+	var num = generate()
+	print(num)
+	enable(fire_array[num]) 
+	
+	
+	
